b082e10edae6507b103010cfea200f18fb68f7d9
Lucas Venezian Povoa <lucasvenez@gmail.com>
2016-07-31 10:57:23 -0300

    Updating layout and text

--- js/main.js
+++ js/main.js
@@ -15,6 +15,27 @@ $(function() {
 			}).done(function(data) {
 				$(".content").html(data);
 			});
+			
+			$(".header menu li a.active").removeClass("active");
+			
+			$(this).addClass("active");
 		})
+	});
+
+	var url = "about.html";
+
+	var section = window.location.hash.substr(1);
+	
+	if (section != "")
+		url = section + ".html";
+	else
+		section = "about";
+
+	$.ajax({
+		url : url,
+		method : "GET"
+	}).done(function(data) {
+		$("a[accesskey=" + section + "]").addClass("active");
+		$(".content").html(data);
 	});
 });
